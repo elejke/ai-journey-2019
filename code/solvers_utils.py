8164211bf1d3f99121db38c10eaf2daaabeb3cdc
elejke <german.novikov@phystech.edu>
2019-09-13 09:45:20 +0300

    solvers utils refactored

--- code/solvers_utils.py
+++ code/solvers_utils.py
@@ -2,17 +2,28 @@ import re
 
 
 def remove_additional(word):
+    """ Function take string of words in w/wo brackets and output only "clean" words
+
+    :param word: string of words like: умный (мальчик)
+    :return: string with removed words in brackets
+    """
     additional_words = re.findall("\([\w\ ]+\)", word)
 
     for additional_word in additional_words:
         word = word.replace(additional_word, "")
-        word = word.strip()
 
-    return word
+    return word.strip()
 
 
 def check_pair(word_1, word_2, big_words_set):
-    # TODO: EMBEDDINGS NEEDED TO USE NOT ONLY WORD BUT WITH CONTEXT
+    """ Check if pair have the same missed letter or not
+
+    :param word_1: first word in pair
+    :param word_2: second word in pair
+    :param big_words_set: set of words to compare with
+    :return: list of letters which can be inserted to both words
+    """
+    # TODO: EMBEDDINGS. NEEDED TO USE NOT ONLY WORD BUT WITH CONTEXT
     word_1 = remove_additional(word_1)
     word_2 = remove_additional(word_2)
     russian_letters = list("абвгдеёжзийклмнопрстуфхцчшщъыьэюя")
