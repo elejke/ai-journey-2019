3a41ddc43c1690718b68cf5746913570ba6938ff
elejke <german.novikov@phystech.edu>
2019-09-17 23:26:19 +0300

    #12 dashboard update on page read + external access fixed

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,42 +1,55 @@
+import os
+import glob
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+import pathlib
+
 import pandas as pd
+import dash_html_components as html
 
-#df = pd.read_csv(
-#    'https://gist.githubusercontent.com/chriddyp/'
-#    'c78bf172206ce24f77d6363a2d754b59/raw/'
-#    'c353e8ef842413cae56ae3920b8fd78468aa4cb2/'
-#    'usa-agricultural-exports-2011.csv')
-
-df_1 = pd.read_csv("./scores_dir/metrics_by_id_1.csv")[["metric"]]
-df_2 = pd.read_csv("./scores_dir/metrics_by_id_2.csv")
-df_1.columns = ["metric_model_1"]
-df_2.columns = ["id", "metric_model_2", "score"]
-
-df = pd.concat([df_2[["id", "score", "metric_model_2"]], df_1], axis=1).round(2)
-
-
-def generate_table(dataframe, max_rows=29):
-    return html.Table(
-        # Header
-        [html.Tr([html.Th(col) for col in dataframe.columns])] +
-
-        # Body
-        [html.Tr([
-            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-        ]) for i in range(min(len(dataframe), max_rows))]
-    )
-
+SCORES_DIR = "../scores_dir/"
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+df_scores = pd.read_csv(os.path.join(SCORES_DIR, "scores.csv"))[["id", "score"]]
+
+df_list = []
+model_list = []
+
+def generate_table(max_rows=29):
+
+    df_path_list = glob.glob(os.path.join(SCORES_DIR, "*.csv"))
+    df_path_list.sort(key=os.path.getmtime)
+
+    for df_path in df_path_list:
+        if "scores.csv" not in df_path:
+            model_id = pathlib.Path(df_path).name.strip(".csv")
+            if model_id not in model_list:
+                df_temp = pd.read_csv(df_path)[["metric"]]
+                df_temp.columns = ["model_" + model_id]
+                df_list.append(df_temp)
+                model_list.append(model_id)
+
+    df = pd.concat([df_scores[["id", "score"]]] + df_list[-5:], axis=1).round(2)
+
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in df.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(df.iloc[i][col]) for col in df.columns
+        ]) for i in range(min(len(df), max_rows))]
+    )
+
+def serve_layout():
+    return html.Div(children=[
+        html.H4(children='Metric by task id'),
+        generate_table()
+    ])
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(children=[
-    html.H4(children='Metric by task id'),
-    generate_table(df)
-])
+app.layout = serve_layout
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True, port=8050, host='0.0.0.0')
