c9af493adfd9c7bb9b2f0df47aeb0a7ae228f639
vladimir-chernykh <vladimir.chernykh@phystech.edu>
2019-10-18 20:30:51 +0300

    add custom solver_27 to combined solution

--- sberbank-baseline/solution.py
+++ sberbank-baseline/solution.py
@@ -12,7 +12,7 @@ from solvers import *
 if "USE_CUSTOM_SOLVERS" in os.environ:
     from src.solvers import solver_1, solver_2, solver_3, solver_4, solver_5, solver_6, solver_7, \
         solver_9, solver_10_11_12, solver_13, solver_14, solver_15, solver_16, solver_17, solver_18, \
-        solver_19, solver_20, solver_21, solver_24, solver_25
+        solver_19, solver_20, solver_21, solver_24, solver_25, solver_27
 
     custom_solvers = {
         1: solver_1,
@@ -36,7 +36,8 @@ if "USE_CUSTOM_SOLVERS" in os.environ:
         20: solver_20,
         21: solver_21,
         24: solver_24,
-        25: solver_25
+        25: solver_25,
+        27: solver_27
     }
 else:
     custom_solvers = {}
